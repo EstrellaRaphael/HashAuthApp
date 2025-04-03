4c93878fdc65a88813cc0612a300b0f6b85dc6f1
EstrellaRaphael <raphaelroestrellar@gmail.com>
2025-04-03 17:11:38 -0300

    Melhoria no formulario de cadastro e estilização do projeto

--- frontend/src/app/components/register/register.component.ts
+++ frontend/src/app/components/register/register.component.ts
@@ -1,11 +1,29 @@
 import { Component } from '@angular/core';
-import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import {
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 import { ApiService, AuthRequest } from '../../services/api.service';
 import { CommonModule } from '@angular/common';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSnackBar, MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-register',
-  imports: [ReactiveFormsModule, CommonModule],
+  imports: [
+    ReactiveFormsModule,
+    CommonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatSnackBarModule,
+    MatIconModule,
+  ],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css',
   standalone: true,
@@ -14,22 +32,38 @@ export class RegisterComponent {
   form: FormGroup;
   message: string | null = null;
   error: boolean = false;
+  showPassword = false;
+  showConfirmPassword = false;
 
-  constructor(private fb: FormBuilder, private api: ApiService) {
+  constructor(
+    private fb: FormBuilder,
+    private api: ApiService,
+    private snackBar: MatSnackBar
+  ) {
     // vai inicializar o formulário com as validações
-    this.form = this.fb.group({
-      email: ['', [Validators.required, Validators.email]],
-      password: [
-        '',
-        [
-          Validators.required,
-          Validators.minLength(8),
-          Validators.pattern(
-            '^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[\\W_]).{8,}$'
-          ),
+    this.form = this.fb.group(
+      {
+        email: ['', [Validators.required, Validators.email]],
+        password: [
+          '',
+          [
+            Validators.required,
+            Validators.minLength(8),
+            Validators.pattern(
+              '^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[\\W_]).{8,}$'
+            ),
+          ],
         ],
-      ],
-    });
+        confirmPassword: ['', Validators.required],
+      },
+      { Validators: this.passwordsMatchValidator }
+    );
+  }
+
+  passwordsMatchValidator(form: FormGroup) {
+    const password = form.get('password')?.value;
+    const confirmPassword = form.get('confirmPassword')?.value;
+    return password === confirmPassword ? null : { passwordMismatch: true}
   }
 
   onSubmit() {
@@ -45,7 +79,7 @@ export class RegisterComponent {
         this.form.reset();
       },
       error: (err) => {
-        this.message = err.message || 'Erro desconhecido';
+        this.message = err.message || 'Erro ao cadastrar.';
         this.error = true;
       },
     });
