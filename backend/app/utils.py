2f9e36cd7c8b84d71fb5d1053d8cf534f83a5a21
EstrellaRaphael <raphaelroestrellar@gmail.com>
2025-04-03 10:01:09 -0300

    Correção da função que cria o hash da senha

--- backend/app/utils.py
+++ backend/app/utils.py
@@ -3,7 +3,7 @@ import re
 
 # vai gerar o hash SHA-1 da senha
 def hash_password(password):
-    return hashlib.sha1(password.encode().hexdigest())
+    return hashlib.sha1(password.encode()).hexdigest()
 
 # vai validar a senha com:
 # min 8 caracteres, min 1 letra maiuscula, min 1 letra minuscula, min 1 numero e min 1 caractere especial
