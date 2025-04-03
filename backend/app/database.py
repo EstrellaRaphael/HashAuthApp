24542fbcc1cd1956b86d2f1e36a41017fa4e0282
EstrellaRaphael <raphaelroestrellar@gmail.com>
2025-04-03 09:23:10 -0300

    Configuração do banco de dados

--- backend/app/database.py
+++ backend/app/database.py
@@ -3,17 +3,35 @@ from flask import current_app, g
 
 # vai criar uma conexao com o banco e salva em um objeto global por request (g)
 
+
 def get_db():
-    pass
+    if "db" not in g:
+        g.db = sqlite3.connect(current_app.config["DATABASE"])
+        g.db.row_factory = sqlite3.Row
+    return g.db
+
 
 # vai fechar a conexão com o banco no final de cada request feito
 def close_db():
-    pass
+    db = g.pop("db", None)
+    if db is not None:
+        db.close()
+
 
 # vai iniciar a estrutura do banco de dados
 def init_db():
-    pass
+    db = get_db()
+    db.execute(
+        """
+            CREATE TABLE IF NOT EXISTS (users
+            email TEXT PRIMARY KEY,
+            password_hash TEXT NOT NULL
+            )
+        """)
+    db.commit()
 
 # vai registrar os metodos acima na aplicação flask
-def init_app():
-    pass
+def init_app(app):
+    app.teardown_appcontext(close_db)
+    with app.app_context():
+        init_db
---
+++ backend/app/utils.py
@@ -0,0 +1,12 @@
+import hashlib
+import re
+
+# vai gerar o hash SHA-1 da senha
+def hash_password(password):
+    return hashlib.sha1(password.encode().hexdigest())
+
+# vai validar a senha com:
+# min 8 caracteres, min 1 letra maiuscula, min 1 letra minuscula, min 1 numero e min 1 caractere especial
+def validate_password(password):
+    pattern = r'^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[\W_]).{8,}$'
+    return re.match(pattern, password)
