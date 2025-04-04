2c9e92c0618d79d4f495804a53fb60ab8835240b
EstrellaRaphael <raphaelroestrellar@gmail.com>
2025-04-04 17:11:52 -0300

    Criação da navbar

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,17 +1,19 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ApiService } from './services/api.service';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true, 
+  imports: [RouterOutlet, NavbarComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'frontend';
+
   constructor(private api: ApiService) {
-    // teste rápido para ver se o serviço está ok
     this.api.getHash('naoexiste@email.com').subscribe({
       next: (res) => console.log('API funcionando:', res),
       error: (err) => console.log('Erro da API:', err)
